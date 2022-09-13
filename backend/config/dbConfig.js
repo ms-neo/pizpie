bcfe13313d7b1c57938d821ab3e952fb9a48227a
ms-neo <sweettoxic111@gmail.com>
2022-09-13 22:46:19 +0300

    update server.js

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-const config = require('config');
+// const config = require('config');
 // const dbUri = config.get('mongoUri');
 const dbUri = process.env.MONGO_URI;
 
 const connectDB = async ()=>{
 
     try {
-        console.log(dbUri)
-        await mongoose.connect(`${dbUri}`,{
+        // console.log(`${dbUri}`)
+        await mongoose.connect(`mongodb+srv://neona:${dbUri}@pizpiedb.powid.mongodb.net/pizpieDB?retryWrites=true&w=majority&ssl=true`,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
--- backend/server.js
+++ backend/server.js
@@ -22,14 +22,14 @@ app.use('/api/payment-stripe', require('./routes/api/paymentRouter'))
 app.use('/api/products', require('./routes/api/productsRouter'))
 app.use('/api/users',require('./routes/api/userRouter'))
 
-if (process.env.NODE_ENV === 'production'){
+// if (process.env.NODE_ENV === 'production'){
     // set static folder
     app.use(express.static("client/build"));
     
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
     });
-    }
+    // }
     
 const port = process.env.PORT || 5000
 
