f36e833352c2bdf1dda1fa70cf880a913a084ea3
ms-neo <sweettoxic111@gmail.com>
2022-09-13 21:20:12 +0300

    fix mongoose uri

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const dbUri = config.get('mongoUri');
+// const dbUri = config.get('mongoUri');
+const dbUri = process.env.MONGO_URI;
 
 const connectDB = async ()=>{
 
