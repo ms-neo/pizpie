4bd98f41338299ab4b2cd090fa4b6f3c22432edb
ms-neo <sweettoxic111@gmail.com>
2022-09-14 15:22:12 +0300

    build react app

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const connectDB = require('./config/dbConfig')
+const path = require('path')
 const cors =require('cors')
 
 const app = express()
@@ -22,14 +23,14 @@ app.use('/api/payment-stripe', require('./routes/api/paymentRouter'))
 app.use('/api/products', require('./routes/api/productsRouter'))
 app.use('/api/users',require('./routes/api/userRouter'))
 
-// if (process.env.NODE_ENV === 'production'){
-//     // set static folder
-//     app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static("client/build"));
     
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
-//     });
-//     }
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+    });
+    }
     
 const port = process.env.PORT || 5000
 
