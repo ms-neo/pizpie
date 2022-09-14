7a6e3e1c0a61c9e1116f110d18c59538115e2c04
ms-neo <sweettoxic111@gmail.com>
2022-09-14 15:05:43 +0300

    FIX node_env

--- backend/server.js
+++ backend/server.js
@@ -23,13 +23,13 @@ app.use('/api/products', require('./routes/api/productsRouter'))
 app.use('/api/users',require('./routes/api/userRouter'))
 
 // if (process.env.NODE_ENV === 'production'){
-    // set static folder
-    // app.use(express.static("client/build"));
+//     // set static folder
+//     app.use(express.static("client/build"));
     
-    // app.get('*', (req, res) => {
-    //   res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
-    // });
-    // }
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+//     });
+//     }
     
 const port = process.env.PORT || 5000
 
