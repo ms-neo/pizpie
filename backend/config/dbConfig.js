96db9bc0ed1813bbf9c7220cce214dec9309dd20
ms-neo <sweettoxic111@gmail.com>
2022-09-14 12:32:55 +0300

    update serever.js

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -7,10 +7,16 @@ const connectDB = async ()=>{
 
     try {
         // console.log(`${dbUri}`)
-        await mongoose.connect(`mongodb+srv://neona:${dbUri}@pizpiedb.powid.mongodb.net/pizpieDB?retryWrites=true&w=majority&ssl=true`,{
+        await mongoose.connect(
+            // `mongodb+srv://neona:${dbUri}@pizpiedb.powid.mongodb.net/test`
+            // `mongodb+srv://neona:${dbUri}@pizpiedb.powid.mongodb.net/?retryWrites=true&w=majority`
+            `mongodb+srv://neona:${dbUri}@pizpiedb.powid.mongodb.net/pizpieDB?retryWrites=true&w=majority&ssl=true`
+        // `mongodb://neona:${dbUri}@endangeredanimals-shard-00-00.eznhk.mongodb.net:27017,endangeredanimals-shard-00-01.eznhk.mongodb.net:27017,endangeredanimals-shard-00-02.eznhk.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-pvwqse-shard-0&authSource=admin&retryWrites=true&w=majority`
+        ,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
+        console.log(`connected to mongodb.....`)
     } catch (err) {
         console.log(err.message, 'err')
         process.exit(1)
