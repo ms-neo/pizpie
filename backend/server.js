ce3dee3b00174af707dd218cb5a4e16bcada7283
ms-neo <sweettoxic111@gmail.com>
2022-09-15 18:02:55 +0300

    fixing dir

--- backend/server.js
+++ backend/server.js
@@ -39,16 +39,16 @@ app.use('/api/payment-stripe', require('./routes/api/paymentRouter'))
 app.use('/api/products', require('./routes/api/productsRouter'))
 app.use('/api/users',require('./routes/api/userRouter'))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './client/build/index.html'));
+//   });
 // if (process.env.NODE_ENV === 'production'){
 //     // set static folder
 //     app.use(express.static("client/build"));
     
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
-//     });
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+    });
 //     }
     
 const port = process.env.PORT || 5000
