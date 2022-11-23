9409b6594ccb45ae171c802121a5d0a247991756
ms-neo <sweettoxic111@gmail.com>
2022-11-23 17:09:54 +0300

    deploying

--- client/build/precache-manifest.457777a43750fe73303ab89321eda7d7.js
+++ client/build/precache-manifest.457777a43750fe73303ab89321eda7d7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "e776e3ea384bb55f8d76",
+    "revision": "2ab9aef29058461ff736",
     "url": "/static/css/main.02f745d4.chunk.css"
   },
   {
-    "revision": "e776e3ea384bb55f8d76",
-    "url": "/static/js/main.796ea5ab.chunk.js"
+    "revision": "2ab9aef29058461ff736",
+    "url": "/static/js/main.3db4f902.chunk.js"
   },
   {
     "revision": "984da704841d0243eb51",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/static/js/3.7bc68c7e.chunk.js"
   },
   {
-    "revision": "e419bd366cfbe85da6ed228298e01555",
+    "revision": "ede5c9caf84c5dde0beed328b4ece169",
     "url": "/index.html"
   }
 ];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4be4466c68c026ee4d645efecdd51ba3.js"
+  "/precache-manifest.457777a43750fe73303ab89321eda7d7.js"
 );
 
 workbox.clientsClaim();
